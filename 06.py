09a3cebf6b1b1d9020c943e133eb3c92c708706e
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-12-31 17:59:48 +0900

    add: 06.py

---
+++ 06.py
@@ -0,0 +1,9 @@
+from z3 import *
+Z = IntSort()
+x = Int('x')
+z = Int('z')
+m, m1 = Array('m', Z, Z), Array('m1', Z, Z)
+def memset(lo, hi, y, m):
+    return Lambda([x], If(And(lo <= x, x <= hi), y, Select(m, x)))
+
+solve([m1 == memset(1, 700, z, m), Select(m1, 6) != z])
